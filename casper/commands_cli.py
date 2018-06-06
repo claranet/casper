917b8b3b3564455f7f4d73325bdd22bbb806a395
Burak Karamahmut <burak.karamahmut@fr.clara.net>
2018-06-06 17:32:15 +0200

    GHOST-640 Added an option to remove ASCII coloration

--- casper/commands_cli.py
+++ casper/commands_cli.py
@@ -20,8 +20,11 @@ from pyghost.api_client import SCRIPT_EXECUTION_STRATEGY_SERIAL
               help="Deployment strategy (default {})".format(DEPLOYMENT_STRATEGY_SERIAL))
 @click.option('--safe-deploy-strategy', type=click.Choice(SAFE_DEPLOYMENT_STRATEGIES),
               help="Safe deployment strategy (default none)")
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def deploy(context, application_id, module, all_modules, strategy, safe_deploy_strategy):
+def deploy(context, application_id, module, all_modules, strategy, safe_deploy_strategy, live_logs, output, no_color):
     # TODO find a "clicker" way to do this parameter validation
     if not module and not all_modules:
         raise MissingParameter('You must have one (and only one) from --module and --all-modules parameters',
@@ -44,7 +47,7 @@ def deploy(context, application_id, module, all_modules, strategy, safe_deploy_s
 
     try:
         job_id = context.jobs.command_deploy(application_id, modules, strategy, safe_deploy_strategy)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -57,11 +60,14 @@ def deploy(context, application_id, module, all_modules, strategy, safe_deploy_s
               help="Deployment strategy (default {})".format(DEPLOYMENT_STRATEGY_SERIAL))
 @click.option('--safe-deploy-strategy', type=click.Choice(SAFE_DEPLOYMENT_STRATEGIES),
               help="Safe deployment strategy (default none)")
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def redeploy(context, application_id, deployment_id, strategy, safe_deploy_strategy):
+def redeploy(context, application_id, deployment_id, strategy, safe_deploy_strategy, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_redeploy(application_id, deployment_id, strategy, safe_deploy_strategy)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -77,13 +83,16 @@ def redeploy(context, application_id, deployment_id, strategy, safe_deploy_strat
               help="Safe deployment strategy (default {})".format(SAFE_DEPLOYMENT_STRATEGY_ONE_BY_ONE))
 @click.option('--instance-ip', help="Instance IP for only one instance execution (default none)")
 @click.option('--module-context', help="Force script working dir from module context (default none)")
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def executescript(context, application_id, script_file, strategy, safe_deploy_strategy, instance_ip, module_context):
+def executescript(context, application_id, script_file, strategy, safe_deploy_strategy, instance_ip, module_context, live_logs, output, no_color):
     try:
         script_content = script_file.read()
         job_id = context.jobs.command_executescript(
             application_id, script_content, strategy, safe_deploy_strategy, instance_ip, module_context)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -93,11 +102,14 @@ def executescript(context, application_id, script_file, strategy, safe_deploy_st
 @click.argument('application-id')
 @click.option('--instance-type', help="Force instance type for build")
 @click.option('--skip-bootstrap', type=bool, help="Force skipping the provisioner bootstrap")
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def buildimage(context, application_id, instance_type, skip_bootstrap):
+def buildimage(context, application_id, instance_type, skip_bootstrap, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_buildimage(application_id, instance_type, skip_bootstrap)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -107,11 +119,14 @@ def buildimage(context, application_id, instance_type, skip_bootstrap):
 @click.argument('application-id')
 @click.option('--subnet-id', help="Force instance subnet id")
 @click.option('--private-ip-address', help="Force private IP address")
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def createinstance(context, application_id, subnet_id, private_ip_address):
+def createinstance(context, application_id, subnet_id, private_ip_address, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_createinstance(application_id, subnet_id, private_ip_address)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -119,11 +134,14 @@ def createinstance(context, application_id, subnet_id, private_ip_address):
 @cli.command('destroyallinstances', short_help='Create a "destroyallinstances" job',
              help="Create a job that destroys all instances for APPLICATION_ID application")
 @click.argument('application-id')
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def destroyallinstances(context, application_id):
+def destroyallinstances(context, application_id, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_destroyallinstances(application_id)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -133,11 +151,14 @@ def destroyallinstances(context, application_id):
 @click.argument('application-id')
 @click.option('--strategy', type=click.Choice(ROLLING_UPDATE_STRATEGIES),
               help="Rolling-update strategy")
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def recreateinstances(context, application_id, strategy):
+def recreateinstances(context, application_id, strategy, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_recreateinstances(application_id, strategy)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -145,11 +166,14 @@ def recreateinstances(context, application_id, strategy):
 @cli.command('updatelifecyclehooks', short_help='Create a "updatelifecyclehooks" job',
              help="Create a job that updates lifecycle hooks APPLICATION_ID application")
 @click.argument('application-id')
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def updatelifecyclehooks(context, application_id):
+def updatelifecyclehooks(context, application_id, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_updatelifecyclehooks(application_id)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -157,11 +181,14 @@ def updatelifecyclehooks(context, application_id):
 @cli.command('updateautoscaling', short_help='Create a "updateautoscaling" job',
              help="Create a job that updates auto scaling for APPLICATION_ID application")
 @click.argument('application-id')
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def updateautoscaling(context, application_id):
+def updateautoscaling(context, application_id, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_updateautoscaling(application_id)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -172,11 +199,14 @@ def updateautoscaling(context, application_id):
 @click.option('--copy-ami', type=bool, help="Copy the AMI from the online application if true.", default=False)
 @click.option('--attach-elb', type=bool, help="Create a temporary ELB to attach to the Auto Scaling group if true.",
               default=True)
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def preparebluegreen(context, application_id, copy_ami, attach_elb):
+def preparebluegreen(context, application_id, copy_ami, attach_elb, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_preparebluegreen(application_id, copy_ami, attach_elb)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -184,11 +214,14 @@ def preparebluegreen(context, application_id, copy_ami, attach_elb):
 @cli.command('purgebluegreen', short_help='Create a "purgebluegreen" job',
              help="Create a job that purges the offline blue-green environment for APPLICATION_ID application")
 @click.argument('application-id')
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def purgebluegreen(context, application_id):
+def purgebluegreen(context, application_id, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_purgebluegreen(application_id)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
 
@@ -198,10 +231,20 @@ def purgebluegreen(context, application_id):
 @click.argument('application-id')
 @click.option('--strategy', type=click.Choice(BLUEGREEN_SWAP_STRATEGIES), default=BLUEGREEN_SWAP_STRATEGY_OVERLAP,
               help="Blue-green swap strategy (default {})".format(BLUEGREEN_SWAP_STRATEGY_OVERLAP))
+@click.option('--live-logs', help="Wait for job starting and output logs", is_flag=True)
+@click.option('--output', help="Path of output log file", type=click.File('w'))
+@click.option('--no-color', help="Remove ANSII color from output", is_flag=True)
 @context
-def swapbluegreen(context, application_id, strategy):
+def swapbluegreen(context, application_id, strategy, live_logs, output, no_color):
     try:
         job_id = context.jobs.command_swapbluegreen(application_id, strategy)
-        click.echo("Job creation OK - ID : {}".format(job_id))
+        handle_job_creation(context, job_id, live_logs, output, no_color)
     except ApiClientException as e:
         raise ClickException(e) from e
+
+
+def handle_job_creation(context, job_id, live_logs=None, output=None, no_color=None):
+  click.echo("Job creation OK - ID : {}".format(job_id))
+  if live_logs:
+    click.echo("Waiting for job logs...")
+    job_log_handler(context, job_id, output, no_color, True)
